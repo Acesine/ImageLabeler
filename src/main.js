441c0a32c4c9474bc0277f931eaa1d66720a2361
acesine <xianggaodave@gmail.com>
2018-04-29 20:48:50 -0700

    Fix icon

--- src/main.js
+++ src/main.js
@@ -13,13 +13,13 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: `file://${__dirname}/../resource/icon.png`
+    icon: `file://${__dirname}/../resource/icon.icns`
   });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow = null;
