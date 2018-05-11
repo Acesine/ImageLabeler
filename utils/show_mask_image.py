db878cd12614819a2d269bbad9aca7124ffa7fa5
acesine <xianggaodave@gmail.com>
2018-05-10 23:06:41 -0700

    Save mask image with utils

--- utils/show_mask_image.py
+++ utils/show_mask_image.py
@@ -3,7 +3,9 @@
 import sys
 import argparse
 import json
+import os
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,24 +15,50 @@ def decompress(compressed_mask):
         r.extend([v[0]] * v[1])
     return r
 
-def main(file_path):
+def combine(a, b, shape):
+    r = np.zeros(shape, dtype=np.uint8)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            r[i, j] = 1 if a[i, j] + b[i, j] > 0 else 0
+    return r
+
+def main(file_path, save_file):
     with open(file_path) as f:
+        file_name = os.path.basename(file_path)
         label_file = json.load(f)
         dimension = label_file['dimension']
         shape_cnt = len(label_file['shapes'])
         cols = min(3, shape_cnt)
         rows = shape_cnt // cols if shape_cnt % cols == 0 else shape_cnt // cols + 1
-        for index, shape in enumerate(label_file['shapes']):
+        # A label may contains multiple parts
+        label_array = {}
+        for shape in label_file['shapes']:
             compressed_mask = shape['mask']
             mask = decompress(compressed_mask)
-            nparray = np.reshape(np.asarray(mask), (dimension[1], dimension[0]))
-            plt.subplot(rows, cols, index+1)
-            plt.title(shape['label'])
-            plt.imshow(nparray, cmap='Greys',  interpolation='nearest')
+            array_shape = (dimension[1], dimension[0])
+            nparray = np.reshape(np.asarray(mask, dtype=np.uint8), array_shape)
+            label_name = shape['label']
+            if label_name not in label_array:
+                label_array[label_name] = nparray
+            else:
+                label_array[label_name] = combine(nparray, label_array[label_name], array_shape)
+
+        index = 1
+        for label_name in label_array:
+            nparray = label_array[label_name]
+            plt.subplot(rows, cols, index)
+            plt.title(label_name)
+            plt.imshow(nparray, cmap='binary')
+            if save_file:
+                label_name = shape['label']
+                matplotlib.image.imsave('{}_{}.png'.format(file_name, label_name), nparray, cmap='binary')
+            index += 1
+
         plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--label-file', dest='label_file', help='label file')
+    parser.add_argument('--save', dest='save', nargs='?', const=True, help='save mask images')
     args = parser.parse_args()
-    main(args.label_file)
+    main(args.label_file, args.save)
